59cabd74c9b7e498101d1da07a989c928aa23053
Imaekhai Oshiokhai <i.oshiokhai733@mybvc.ca>
2026-04-08 08:37:19 -0600

    initial react app with name

---
+++ src/App.test.jsx
@@ -0,0 +1,7 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders my name on the page', () => {
+  render(<App />);
+  expect(screen.getByText('Imaekhai')).toBeInTheDocument();
+});
